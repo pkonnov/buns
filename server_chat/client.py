74feded407e9affbeda39828c02d181589943f40
cretyne <4itonada@gmail.com>
2018-04-02 21:25:39 +0400

    watch leter

---
+++ server_chat/client.py
@@ -0,0 +1,31 @@
+import socket, threading, time
+
+key = 8194
+
+shutdown = False
+join = False
+
+def receving(name, sock):
+	while not shutdown:
+		try:
+			while True:
+				data, addr = sock.recvfrom(1024)
+				# print(data.decode('utf-8'))
+
+				# begin
+				decrypt = ''; k = False
+				for i in data.decode('utf-8'):
+					if i == ':':
+						k = True
+						decrypt += i
+					elif k == False or i == ' ':
+						decrypt += i
+					else:
+						decrypt += chr(ord(i)^key)
+				print(decrypt)
+				# end
+
+				time.sleep(0.2)
+
+		except:
+			pass
---
+++ server_chat/servers.py
@@ -0,0 +1,34 @@
+import socket, time
+
+host = socket.gethostbyname(socket.gethostname()) # 192.168.0.1
+port = 9090 # not admins pasport
+
+clients = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #tcp ip
+s.bind((host,port)) # up server
+
+quit = False
+print('[server startet]')
+
+while not quit:
+	try:
+		data, addr = s.recvfrom(1024)
+
+		if addr not in clients:
+			clients.append(addr)
+
+		itsatime = time.strftime('%Y-%m-%d-%H.%M.%S', time.localtime())
+
+		print('[' + addr[0] + ']=[' + str(addr[1]) + ']=[' + itsatime + ']/', end='')
+		print(data.decode('utf-8'))
+
+		for client in clients:
+			if addr != client:
+				s.sendto(data, client)
+
+	except:
+		print('\n[ server stoped ]')
+		qiut = True
+
+s.close()
