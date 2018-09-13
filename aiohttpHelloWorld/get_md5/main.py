1ce47f8399a655afc8813e405bfce455c3b04ab5
Pavel Konnov <4itonada@gmail.com>
2018-09-13 19:36:32 +0400

    next

--- aiohttpHelloWorld/get_md5/main.py
+++ aiohttpHelloWorld/get_md5/main.py
@@ -1,17 +1,18 @@
+#!/usr/bin/python3
 from aiohttp import web
 import hashlib
 
-async def index(request):
-	return web.Response(text='no')
 
 async def talk_url(request):
-    summ = '100'
-    id_u = '001'
-    hash_md = hashlib.md5((summ + id_u).encode('utf-8')).hexdigest()
-    return web.Response(text=hash_md)
+	summ = '100'
+	id_u = '001'
+	q = '?'
+	hash_md = hashlib.md5((summ + id_u).encode('utf-8')).hexdigest()
+	return web.Response(text=hash_md)
 
 app = web.Application()
-app.add_routes([web.get('/', index),
-                web.get('/uri?({hash_md})', talk_url)])
+app.router.add_route('GET', '/uri?sum={summ}&id={id_u}&hash={hash_md}', talk_url)
 
-web.run_app(app)
+
+if __name__ == '__main__':
+	web.run_app(app)
