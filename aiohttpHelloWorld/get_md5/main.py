c498ebd681e317e2568bad5018abf8da525316e0
Pavel Konnov <4itonada@gmail.com>
2018-09-17 20:48:32 +0400

    generated the link, now you need to catch the parameters and generate the hash

--- aiohttpHelloWorld/get_md5/main.py
+++ aiohttpHelloWorld/get_md5/main.py
@@ -1,18 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.5
 from aiohttp import web
 import hashlib
 
 
 async def talk_url(request):
-	summ = '100'
-	id_u = '001'
-	q = '?'
-	hash_md = hashlib.md5((summ + id_u).encode('utf-8')).hexdigest()
-	return web.Response(text=hash_md)
 
-app = web.Application()
-app.router.add_route('GET', '/uri?sum={summ}&id={id_u}&hash={hash_md}', talk_url)
+    # /uri?sums=sums&id_u=id_u&hash=hash_md
+
+    sums = request.rel_url.query['sums']
+    id_u = request.rel_url.query['id_u']
+    # hash_md = hashlib.md5((sums + id_u).encode('utf-8')).hexdigest()
+    param_hash = request.rel_url.query['param_hash']
+    params = "sums:{}, id_u:{}, hash:{}".format(sums, id_u, param_hash)
+    return web.Response(text=str(params))
 
 
 if __name__ == '__main__':
-	web.run_app(app)
+    app = web.Application()
+    app.router.add_route('GET', "/uri", talk_url)
+    web.run_app(app)
